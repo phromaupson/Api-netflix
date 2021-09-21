25b5e9283a3255db1b2bbf1ae30e4c16058b7201
kamkonphrom <kamkonphrom@gmail.com>
2021-09-21 21:47:48 +0700

    add fuction register and encrypt password

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const User = require("../models/User");
-
-//REGISTER
-router.post("/register", (req, res) => {
-    const newUser = new User({
-        username: req.body.username
-    })
-})
